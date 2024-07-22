29ac027ba6bf3466832bb327ff6780bc7da3f04e
Lucas <lucashenriqueslima@outlook.com>
2024-07-22 16:59:32 -0300

    fish web.php

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,16 @@ use STS\ZipStream\Facades\Zip;
 use STS\ZipStream\Models\S3File;
 use Illuminate\Support\Facades\Response as Download;
 
-// Route::get('/auvo', function () {
-//     $auvoService = new AuvoService();
-//     [$solidyCustomers, $motoclubCustomers] = $auvoService->getIlevaDatabaseCustomers();
+Route::get('/auvo', function () {
+    $auvoService = new AuvoService();
+    [$solidyCustomers, $motoclubCustomers] = $auvoService->getIlevaDatabaseCustomers();
 
-//     $auvoService->updateCustomers($solidyCustomers);
-//     $auvoService->updateCustomers($motoclubCustomers, 'mc');
-// });
+    $auvoService->updateCustomers($solidyCustomers);
+    $auvoService->updateCustomers($motoclubCustomers, 'mc');
+});
 
-// Route::get('/field-control', function () {
-//     $customers = IlevaAssociateVehicle::getVehiclesForFieldControl();
+Route::get('/field-control', function () {
+    $customers = IlevaAssociateVehicle::getVehiclesForFieldControl();
 
     foreach ($customers as $customer) {
 
@@ -33,20 +33,3 @@ use Illuminate\Support\Facades\Response as Download;
 });
 
 Route::get('/call/{call}/download', [CallController::class, 'download'])->name('call.download');
-
-// Route::get('/call/{call}/download', function (Call $call) {
-
-
-//     return Zip::create('call.zip')
-//         ->addFromDisk('s3', 'public/expertise/1UoRFktivJKfcwYe5V4NQ2NN2SANGGGEvhYXTOm8.jpg', 'image.jpg');
-
-//     $call::with('expertises')->get();
-
-//     $call->expertises->each(function ($expertise) {
-//         $expertise->files->each(function ($file) {
-//             $contents = Storage::disk('s3')->get($file->path);
-//             $localPath = '/home/lucas/projetos-laravel/utiliza-api/storage/app/public' . '/' . basename($file->path);
-//             file_put_contents($localPath, $contents);
-//         });
-//     });
-// })
