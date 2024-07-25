ff1c5f7d692094fa898aea5c3c2073e4163d4d50
Lucas <lucashenriqueslima@outlook.com>
2024-07-25 10:20:32 -0300

    fix: database notification script

--- resources/views/filament/body-end.blade.php
+++ resources/views/filament/body-end.blade.php
@@ -1,4 +1,4 @@
-<footer class="fixed bottom-0 left-0 z-20 w-full text-center p-1 bg-white border-t border-gray-200 shadow md:p-6 dark:bg-gray-800 dark:border-gray-600">
+<footer class="fixed bottom-0 left-0 z-20 w-full text-center bg-white border-t border-gray-200 shadow md:p-6 dark:bg-gray-800 dark:border-gray-600">
     <span class="text-xs text-gray-500 text-center dark:text-gray-400">© 2024
         <a href="https://www.linkedin.com/in/lucas-henrique-souza-4395441b7/" class="hover:underline">Luc Tech Solutions™</a> All Rights Reserved.
     </span>
@@ -8,8 +8,15 @@
     setInterval(checkNotificationAudio, 30000);
 
     async function checkNotificationAudio() {
+
     try {
-        let unreadedNotifications =  await Livewire.all()[0].$wire.getUnreadNotificationsCount()
+        let databaseNotificationComponent = Livewire.all()[0]
+
+        if(databaseNotificationComponent.name != 'filament.livewire.database-notifications') {
+            return;
+        }
+
+        let unreadedNotifications =  await databaseNotificationComponent.$wire.getUnreadNotificationsCount()
         if(unreadedNotifications > 0){
             let audio = new Audio('{{url('/sound/notification.mp3')}}');
             audio.play();
