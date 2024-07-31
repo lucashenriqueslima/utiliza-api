1870e2fbac39421fc66c3cdf8b26482b1c1bec87
Lucas <lucashenriqueslima@outlook.com>
2024-07-31 00:52:07 -0300

    fix: showReason function

--- app/Http/Controllers/BikerChangeCallController.php
+++ app/Http/Controllers/BikerChangeCallController.php
@@ -12,22 +12,14 @@ class BikerChangeCallController extends Controller
     public function showReason(string $callId)
     {
 
-        try {
-            return response()->json(['reason' => BikerChangeCall::where('call_id', $callId)
-                ->where('user_id', auth()->id())
-                ->firstOrFail()->reason]);
-        } catch (\Throwable $th) {
+        $bikerChangeCall = BikerChangeCall::where('call_id', $callId)
+            ->where('user_id', auth()->id())
+            ->first();
+
+        if (!$bikerChangeCall) {
             return response()->json(['reason' => ''], 404);
         }
 
-        // $bikerChangeCall = BikerChangeCall::where('call_id', $callId)
-        //     ->where('user_id', auth()->id())
-        //     ->first();
-
-        // if (!$bikerChangeCall) {
-        //     return response()->json(['reason' => ''], 404);
-        // }
-
-        // return response()->json(['reason' => $bikerChangeCall->reason]);
+        return response()->json(['reason' => $bikerChangeCall->reason]);
     }
 }
