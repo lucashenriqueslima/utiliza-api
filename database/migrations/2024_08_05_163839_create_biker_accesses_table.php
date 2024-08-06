b89d8cc0a4713084721826ab932b9d749c545cfb
Lucas <lucashenriqueslima@outlook.com>
2024-08-06 13:21:59 -0300

    fix: removed duplicated id

--- database/migrations/2024_08_05_163839_create_biker_accesses_table.php
+++ database/migrations/2024_08_05_163839_create_biker_accesses_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('biker_accesses', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->foreignId('biker_id')->constrained('bikers');
             $table->string('device_id');
             $table->string('device_name');
