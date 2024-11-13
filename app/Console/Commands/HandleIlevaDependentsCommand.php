c93630507f90b352cf9eaf7904166e080a2ffdba
Lucas Henrique <lucashenriqueslima@outlook.com>
2024-11-12 23:10:48 -0300

    att

--- app/Console/Commands/HandleIlevaDependentsCommand.php
+++ app/Console/Commands/HandleIlevaDependentsCommand.php
@@ -41,7 +41,7 @@ class HandleIlevaDependentsCommand extends Command
                     && strlen($item['resposta']) > 6
                     && preg_match('/[a-zA-Z]/', $item['resposta'])
                     && stripos($item['resposta'], 'não') === false
-                    && stripos($item['resposta'], 'nao') === false;;
+                    && stripos($item['resposta'], 'nao') === false;
             });
 
             foreach ($dependents as $dependent) {
@@ -51,9 +51,9 @@ class HandleIlevaDependentsCommand extends Command
                         'association' => $ilevaAssociteWithDependents['association'],
                     ],
                     [
-                        'situation' => $ilevaAssociteWithDependents['situacao'],
+                        'situation' => $ilevaAssociteWithDependents['situacao'] ?? 'Não Informado',
                         'benefit' => $ilevaAssociteWithDependents['beneficio'],
-                        'contract_date' => $ilevaAssociteWithDependents['contract_date']
+                        'contract_date' => $ilevaAssociteWithDependents['contract_date'],
                     ]
                 );
             }
@@ -62,6 +62,22 @@ class HandleIlevaDependentsCommand extends Command
 
     private function updateOrCreateDependentAssociate(array $associate): Associate
     {
+
+        if (empty($associate['id_associado'])) {
+            return Associate::updateOrCreate(
+                [
+                    'cpf' => $associate['cpf'],
+                    'name' => $associate['associado'],
+                    'ileva_associate_id' => null
+                ],
+                [
+                    'name' => $associate['associado'],
+                    'email' => $associate['email'],
+                    'phone' => $associate['tel_celular'],
+                ]
+            );
+        }
+
         return Associate::updateOrCreate(
             ['ileva_associate_id' => $associate['id_associado']],
             [
@@ -84,7 +100,6 @@ class HandleIlevaDependentsCommand extends Command
     hap.email,
     hap.tel_celular,
     hap.cpf,
-
     hav.id_plan_item,
     hapiv.id_plan,
     hapiv.val_mensal AS valor_plan,
@@ -128,6 +143,37 @@ WHERE habv.id_beneficio IN (319,585, 42, 43, 50, 131, 175, 207, 237, 238, 267, 2
 AND hait.perguntas_contrato LIKE '%{[dependente%'
 ");
 
-        return json_decode(json_encode($ilevaAssociatesWithDependets), true);
+        $ilevaAssociatesWithDependetsArray = json_decode(json_encode($ilevaAssociatesWithDependets), true);
+
+        $ilevaAssociatesWithDependetsAfterContract = DB::connection('ileva')
+            ->select("
+SELECT
+hai.nome as associado,
+hai.create_at as contract_date,
+hai.cpf_cnpj as cpf,
+hab.nome AS beneficio,
+hait.perguntas_contrato,
+hai.telefone tel_celular,
+hai.email,
+'solidy' AS association
+FROM  hbrd_adm_indication hai
+LEFT JOIN hbrd_adm_indication_termo hait ON hait.id_indicacao = hai.id
+LEFT JOIN hbrd_adm_plan_item hapi ON hapi.id = hai.id_plan_item
+LEFT JOIN hbrd_adm_plan hap ON hap.id = hapi.id_plan
+LEFT JOIN hbrd_adm_benefit_indication habi ON habi.id_indication = hai.id
+LEFT JOIN hbrd_adm_benefit hab ON hab.id = habi.id_benefit
+WHERE
+    hai.modelo IN ('Elba Weekend 1.5 i.e. 2p e 4p',
+                   'Elba 1.6i.e/Top/CSL/ 1.6i.e/1.5 2p e 4p',
+                   'Elba S 1.6/ 1.5ie / 1.5 / 1.3')
+    AND hai.classificacao = 'arquivada'
+    AND hab.nome IS NOT NULL
+    AND hait.perguntas_contrato IS NOT NULL
+    AND hab.id IN (319,585, 42, 43, 50, 131, 175, 207, 237, 238, 267, 277, 280, 309, 310, 335, 366, 416, 425, 427, 688, 68, 69, 70, 71, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 127, 128, 525, 839)
+");
+
+        $ilevaAssociatesWithDependetsAfterContractArray = json_decode(json_encode($ilevaAssociatesWithDependetsAfterContract), true);
+
+        return array_merge($ilevaAssociatesWithDependetsArray, $ilevaAssociatesWithDependetsAfterContractArray);
     }
 }
