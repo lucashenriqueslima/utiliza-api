3850bd33d84f19a44dac4a4b0c6a3c49e3fa2efa
Lucas Henrique <lucashenriqueslima@outlook.com>
2024-12-10 16:49:01 -0300

    att

--- app/Console/Commands/HandleIlevaDependentsCommand.php
+++ app/Console/Commands/HandleIlevaDependentsCommand.php
@@ -163,10 +163,7 @@ SELECT
 hai.nome as associado,
 hai.create_at as contract_date,
 hai.cpf_cnpj as cpf,
-CASE
-	WHEN (hap.nome = 'PLANO AUPET HEINSTEN - VITAL (39,90)' AND hab.nome = 'FUTURA ASSISTÊNCIA + 3 Dependentes') THEN 'PLANO AUPET HEINSTEN - VITAL (39,90)'
-	ELSE IFNULL(hab.nome, hap.nome)
-END beneficio,
+hab.nome beneficio,
 hait.perguntas_contrato,
 hai.telefone tel_celular,
 hai.email,
@@ -180,18 +177,126 @@ LEFT JOIN hbrd_adm_benefit hab ON hab.id = habi.id_benefit
 WHERE
     hai.modelo IN ('Elba Weekend 1.5 i.e. 2p e 4p',
                    'Elba 1.6i.e/Top/CSL/ 1.6i.e/1.5 2p e 4p',
-                   'Elba S 1.6/ 1.5ie / 1.5 / 1.3',
-                   'Aupet')
+                   'Elba S 1.6/ 1.5ie / 1.5 / 1.3')
     AND hai.classificacao = 'arquivada'
+    AND hait.`status` = 'aprovada'
     AND hait.perguntas_contrato IS NOT NULL
-    AND (
-	 	hab.id IN (51, 52, 53, 811, 812, 813, 991, 992, 993, 995, 1027, 1028, 1029, 1030, 1031, 1032, 1033, 1034, 1035, 1036, 1037, 1038,  319,585, 42, 43, 50, 131, 175, 207, 237, 238, 267, 277, 280, 309, 310, 335, 366, 416, 425, 427, 688, 68, 69, 70, 71, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 127, 128, 525, 839)
-	 	OR hap.id IN (250,251,252,254,255,256)
-	 )
+    AND hab.id IN (51, 52, 53, 811, 812, 813, 991, 992, 993, 995, 1027, 1028, 1029, 1030, 1031, 1032, 1033, 1034, 1035, 1036, 1037, 1038,  319,585, 42, 43, 50, 131, 175, 207, 237, 238, 267, 277, 280, 309, 310, 335, 366, 416, 425, 427, 688, 68, 69, 70, 71, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 127, 128, 525, 839)
+
 ");
 
         $ilevaAssociatesWithDependetsAfterContractArray = json_decode(json_encode($ilevaAssociatesWithDependetsAfterContract), true);
 
-        return array_merge($ilevaAssociatesWithDependetsArray, $ilevaAssociatesWithDependetsAfterContractArray);
+
+        $ilevaSolidyAssociatesWithDependetsAupetBenefit = DB::connection('ileva')
+            ->select('
+                SELECT
+hav.id,
+hav.id_associado,
+hap.nome as associado,
+hav.id_plan_item,
+hapiv.id_plan,
+hapiv.val_mensal as valor_plan,
+hav.id_situacao,
+has.nome as situacao,
+hav.id_consultor,
+hac.nome as consultor,
+hat.equipe,
+hasc.nome as regional,
+hav.placa,
+hav.chassi,
+hav.dt_contrato,
+hmuc.cidade,
+hmus.estado,
+hmus.uf,
+habv.id_beneficio,
+CONCAT("SOLIDY_", habv.id_beneficio) AS assoc_id_beneficio,
+habv.id_veiculo,
+habv.created_at,
+habv.valor_beneficio,
+hab.nome as beneficio,
+hab.valor,
+hab.delete_at as delet_banco_dados,
+hait.perguntas_contrato,
+"Solidy" as Associacao
+FROM hbrd_asc_veiculo hav
+LEFT JOIN hbrd_asc_associado haa on haa.id = hav.id_associado
+LEFT JOIN hbrd_asc_pessoa hap on hap.id = haa.id_pessoa
+LEFT JOIN hbrd_asc_situacao has on has.id = hav.id_situacao
+LEFT JOIN hbrd_main_util_city hmuc on hmuc.id = hap.id_cidade
+LEFT JOIN hbrd_main_util_state hmus on hmus.id = hmuc.id_estado
+LEFT JOIN hbrd_asc_beneficio_veiculo habv on habv.id_veiculo = hav.id
+LEFT JOIN hbrd_adm_benefit hab ON hab.id = habv.id_beneficio
+LEFT JOIN hbrd_adm_consultant hac on hac.id = hav.id_consultor
+LEFT JOIN hbrd_adm_team hat on hat.id = hac.id_equipe_
+LEFT JOIN hbrd_adm_sectional hasc on hasc.id = hat.id_regional
+LEFT JOIN hbrd_adm_plan_item hapiv  on hapiv.id = hav.id_plan_item
+LEFT JOIN hbrd_adm_indication hai on hai.id = hav.id_indicacao
+LEFT JOIN hbrd_adm_indication_termo hait on hait.id = hai.id_termo
+WHERE hav.id_situacao = "1"
+AND LOWER(hab.nome) LIKE "%aupet%" OR LOWER(hab.nome) LIKE "%au pet%"
+');
+
+        $ilevaSolidyAssociatesWithDependetsAupetBenefitArray = json_decode(json_encode($ilevaSolidyAssociatesWithDependetsAupetBenefit), true);
+
+
+        $ilevaMotoclubAssociatesWithDependetsAupetBenefit = DB::connection('ileva_motoclub')
+            ->select('
+                                SELECT
+hav.id,
+hav.id_associado,
+hap.nome as associado,
+hav.id_plan_item,
+hapiv.id_plan,
+hapiv.val_mensal as valor_plan,
+hav.id_situacao,
+has.nome as situacao,
+hav.id_consultor,
+hac.nome as consultor,
+hat.equipe,
+hasc.nome as regional,
+hav.placa,
+hav.chassi,
+hav.dt_contrato,
+hmuc.cidade,
+hmus.estado,
+hmus.uf,
+habv.id_beneficio,
+CONCAT("SOLIDY_", habv.id_beneficio) AS assoc_id_beneficio,
+habv.id_veiculo,
+habv.created_at,
+habv.valor_beneficio,
+hab.nome as beneficio,
+hab.valor,
+hab.delete_at as delet_banco_dados,
+hait.perguntas_contrato,
+"Solidy" as Associacao
+FROM hbrd_asc_veiculo hav
+LEFT JOIN hbrd_asc_associado haa on haa.id = hav.id_associado
+LEFT JOIN hbrd_asc_pessoa hap on hap.id = haa.id_pessoa
+LEFT JOIN hbrd_asc_situacao has on has.id = hav.id_situacao
+LEFT JOIN hbrd_main_util_city hmuc on hmuc.id = hap.id_cidade
+LEFT JOIN hbrd_main_util_state hmus on hmus.id = hmuc.id_estado
+LEFT JOIN hbrd_asc_beneficio_veiculo habv on habv.id_veiculo = hav.id
+LEFT JOIN hbrd_adm_benefit hab ON hab.id = habv.id_beneficio
+LEFT JOIN hbrd_adm_consultant hac on hac.id = hav.id_consultor
+LEFT JOIN hbrd_adm_team hat on hat.id = hac.id_equipe_
+LEFT JOIN hbrd_adm_sectional hasc on hasc.id = hat.id_regional
+LEFT JOIN hbrd_adm_plan_item hapiv  on hapiv.id = hav.id_plan_item
+LEFT JOIN hbrd_adm_indication hai on hai.id = hav.id_indicacao
+LEFT JOIN hbrd_adm_indication_termo hait on hait.id = hai.id_termo
+WHERE hav.id_situacao = "1"
+AND LOWER(hab.nome) LIKE "%aupet%" OR LOWER(hab.nome) LIKE "%au pet%"
+            ');
+
+        $ilevaMotoclubAssociatesWithDependetsAupetBenefitArray = json_decode(json_encode($ilevaMotoclubAssociatesWithDependetsAupetBenefit), true);
+
+        //merge all arrays and return
+        return array_merge(
+            $ilevaAssociatesWithDependetsArray,
+            $ilevaAssociatesWithDependetsAfterContractArray,
+            $ilevaSolidyAssociatesWithDependetsAupetBenefitArray,
+            $ilevaMotoclubAssociatesWithDependetsAupetBenefitArray
+        );
     }
 }
